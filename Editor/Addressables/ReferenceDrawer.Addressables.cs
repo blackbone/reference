fe5b594bf6f483ed75f4cc6f421951c7fd4eee28
Dmytro Osipov <blackboneworks@gmail.com>
2023-02-22 21:46:32 +0100

    initial addressables support
    > move addressables assets editor out of ignored folders

--- Editor/Addressables/ReferenceDrawer.Addressables.cs
+++ Editor/Addressables/ReferenceDrawer.Addressables.cs
@@ -1,20 +1,21 @@
-﻿namespace References.Editor
-{
-    using System.Diagnostics;
-    using UnityEditor;
-    using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
-    public abstract partial class ReferenceDrawer
+namespace References.Editor
+{
+    public abstract class ReferenceDrawer
     {
         private static void ModifyAddressableRect(Object asset, ref Rect addressableRect, ref Rect objectFieldRect)
         {
             if (asset != null)
             {
-                addressableRect       =  new Rect(objectFieldRect.x + objectFieldRect.width - 23, objectFieldRect.y, 23, objectFieldRect.height);
+                addressableRect = new Rect(objectFieldRect.x + objectFieldRect.width - 23, objectFieldRect.y, 23, objectFieldRect.height);
                 objectFieldRect.width -= 23;
             }
             else
+            {
                 addressableRect = Rect.zero;
+            }
         }
 
         private static void DrawAddressablesControl(Rect rect, string assetGuid, Object asset)
@@ -25,7 +26,7 @@
                 ? isImplicitlyAdded
                     ? EditorGUIUtility.IconContent("icons/processed/d_prefabvariant icon.asset", $"|Is implicit addressable ({assetEntry.address})")
                     : EditorGUIUtility.IconContent("icons/processed/d_prefabmodel icon.asset", $"|Is addressable ({assetEntry.address}) ")
-                : EditorGUIUtility.IconContent("d_console.warnicon.sml", $"|Not addressable! Link or add to addressables by click.");
+                : EditorGUIUtility.IconContent("d_console.warnicon.sml", "|Not addressable! Link or add to addressables by click.");
 
             if (!GUI.Button(rect, iconContent, EditorStyles.toolbarButton)) return;
             if (!isAddressable || isImplicitlyAdded) AddressableUtility.AddToAddressables(asset, null, AssetDatabase.GetAssetPath(asset));
